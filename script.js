9182b8898ba6bf8cd7f71517e582cf03193d11c5
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-07-15 17:40:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
 //your JS code here. If required.
+const btn = document.querySelector("#btn");
+const output =document.querySelector("#output");
+
+btn.addEventListener("click",()=>{
+	const inputVal = document.querySelector("#ip").value;
+
+	const firstPromise = new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				resolve(Number(inputVal));
+			,2000})
+	});
+
+	// Chain Promise
+
+	firstPromise.then((number)=>{
+		output.textContent = `Result : ${number}`;
+
+		return new Promise((resolve,reject)=>{
+
+			setTimeout(()=>{
+				resolve(number * 2);
+			},2000)
+		})
+	}).then(number=>{
+		output.textContent = `Result :${number}`;
+
+		return new Promise((resolve,reject)=>{
+
+			setTimeout(()=>{
+				resolve(number - 3);
+			},2000)
+		})
+	}).then(number=>{
+		output.textContent = `Result: ${number}`
+
+		return new Promise((resolve,reject)=>{
+
+			setTimeout(()=>{
+				resolve(number / 2);
+			},2000)
+		})
+	}).then(number=>{
+		output.textContent = `Result ; ${number}`;
+
+		return new Promise ((resolve,reject)=>{
+			setTimeout(()=>{
+				resolve(number+ 10)
+			},2000)
+		})
+	}).then(finalResult=.{
+			output.textContent= `Final Result : ${finalResult}`
+	}).catch((err)=>{
+	console.error("Error:",err);
+	})
+
+})
